5621c5111c43bd3cee96ff88ef94d0c00516a754
Frank-Owusu-Ansah <frankoa1118@gmail.com>
2022-12-08 18:24:10 -0800

    Hello, puts

---
+++ 0x00-hello_world/.emacs
@@ -0,0 +1,8 @@
+(setq c-default-style "bsd"
+      c-basic-offset 8
+      tab-width 8
+      indent-tab-mode t)
+(require 'whitespace)
+(setq whitespace-style '(face empty-lines trailing)
+      (global-whitespace-mode t)
+      (setq column-number-mode t)
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
+
+
 
 /**
- * main - print a string
- * Return: 0 if exited properly,non-zero otherwise
- */
+ *
+ *  *main - print a string
+ *
+ *   *Return: 0 if exited properly, non-zero otherwise
+ *
+ *    */
+
 int main(void)
+
 {
-	puts("Programming is like building a multillingual puzzle")
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
 }
