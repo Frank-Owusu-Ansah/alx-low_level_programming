ffbfdcb496f0df53864dade97d02e21bd298ad66
Frank-Owusu-Ansah <frankoa1118@gmail.com>
2022-12-20 16:58:06 -0800

    9-strcpy.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,37 +1,17 @@
-#include"main.h"
-
-
+#include "main.h"
+#include <stdio.h>
 
 /**
-
- * _puts - prints a string, followed by a newline to stdout
-
+ * _puts - prints a strings
  *
-
- * @str: string parameter to print
-
- *
-
- * Return: Nothing
-
+ * @str: pointer
  */
 
-
-
 void _puts(char *str)
-  
 {
-  
-  while (*str != '\0')
-    
-    {
-      
-      _putchar(*str + 0);
-      
-      ++str;
-      
-    }
-  
-  _putchar('\n');
-  
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
 }
