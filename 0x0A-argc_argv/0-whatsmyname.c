684e0ce5b23371d220f4a178acf316064391d9d0
Frank-Owusu-Ansah <frankoa1118@gmail.com>
2023-01-12 18:18:12 -0800

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,27 +1,15 @@
+#include "main.h"
 #include <stdio.h>
-
-
-
+#define UNUSED(x) (void)(x)
 /**
- *
- *  * main - prints its name, followed by a new line.
- *
- *   * @argc: number of command line arguments.
- *
- *    * @argv: array that contains the program command line arguments.
- *
- *     * Return: 0 - success.
- *
- *      */
-
-
-
-int main(int argc __attribute__((unused)), char *argv[])
-
+ * main - main function
+ * @argc: argumentc
+ * @argv: vector of arguments
+ *Return: always 0
+ */
+int main(int argc, char *argv[])
 {
-
-		printf("%s\n", argv[0]);
-
-			return (0);
-
+	UNUSED(argc);
+	printf("%s\n", argv[0]);
+	return (0);
 }
